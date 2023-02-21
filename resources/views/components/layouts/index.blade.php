7c0b5711880d57826a403674043f91962a361f1c
Gacs Peter <gacs.peter@mmsone.com>
2023-02-21 12:14:50 +0100

    add basic footer

--- resources/views/components/layouts/index.blade.php
+++ resources/views/components/layouts/index.blade.php
@@ -4,8 +4,12 @@
             <x-sidemenu/>
         </div>
     </div>
-    <div class="flex-1 flex flex-col relative ml-60 scroll-smooth scroll-pt-16">
+    <div class="flex-1 flex flex-col relative ml-60 scroll-smooth scroll-pt-16 mb-16">
         <div class="flex-1 px-8 py-4">
             {{ $slot }}
         </div>
+    </div>
+    <div class="fixed left-60 bottom-0 right-0">
+        <x-footer/>
+    </div>
 </x-layouts.base>
