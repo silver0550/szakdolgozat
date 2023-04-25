34f3b57dc3fc6ccad613e42d3c551aef030558e7
Gacs Peter <gacs.peter@mmsone.com>
2023-04-25 11:13:08 +0200

    fix admins log, fix admin factory

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,10 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+
 
 class Admin extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logAll();
+    }
 
     protected $fillable = [
         'user_id',
