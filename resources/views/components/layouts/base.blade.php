2d06b2f3b4bb3f4326352eaa3862c6e35957873a
Gacs Péter <g.silver.014@gmail.com>
2023-09-10 19:43:08 +0200

    build new alert system

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Corp</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.css" integrity="sha512-wJgJNTBBkLit7ymC6vvzM1EcSWeM9mmOu+1USHaRBbHkm6W9EgM0HY27+UtUaprntaYQJF75rc8gjxllKs5OIQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         @stack('styles')
         {{-- @vite('vendor/tinymce/tinymce/tinymce.min.js') --}}
         @vite([
@@ -13,6 +14,9 @@
         ])
         <script src="https://cdn.tailwindcss.com"></script>
         <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js" integrity="sha512-zlWWyZq71UMApAjih4WkaRpikgY9Bz1oXIW5G0fED4vk14JjGlQ1UmkGM392jEULP8jbNMiwLWdM8Z87Hu88Fw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         {{-- <x-admin.theme /> --}}
 
         @livewireStyles
@@ -24,16 +28,8 @@
         @stack('scripts')
 
         @livewire('livewire-ui-modal')
-        @livewireScripts
-
-        {{-- NOTIFICATION MODELS --}}
-
-        <x-notifications.successful/>
-    
-        <x-notifications.faild/>
-
-        {{-- NOTIFICATION END --}}
-        
+        @livewireScripts    
        
     </body>
+    <x-notification/>
 </html>
