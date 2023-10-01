3a30ec8d6c90c733b0bffdf9f563f6a07bbdf9b3
Gacs Peter <g.silver.014@gmail.com>
2023-10-01 12:07:26 +0200

    init roles page

--- app/Http/Livewire/Roles.php
+++ app/Http/Livewire/Roles.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Roles extends Component
 {
-    public function render()
+    public $userId;
+    public function render(): View
     {
         return view('livewire.roles')->layout('components.layouts.index');
     }
--- resources/views/livewire/roles.blade.php
+++ resources/views/livewire/roles.blade.php
@@ -1,3 +1,24 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    <div class="flex justify-between w-full p-2 bg-base-200 rounded-md">
+        <div class="ml-4 mb-4 w-1/2">
+            <x-selector
+                :label="'Felhasználó'"
+                wire:model="userId">
+                <option selected value="{{null}}">{{ __('global.select') }}</option>
+                @foreach (\App\Models\User::all() as $user)
+                    <option value={{ $user->id }}>{{ $user->name }}</option>
+                @endforeach
+            </x-selector>
+        </div>
+        <x-input.form-control class="w-1/2 pr-2" label='Szerepkör'>
+            <label class="label" for="">
+                <span class="label-text">
+                    {{ collect(\App\Models\User::find($userId)?->getRoleNames())->first() ?? 'Nincs szerepkör kiosztva' }}
+                </span>
+            </label>
+        </x-input.form-control>
+    </div>
+    <div class="flex justify-between w-full p-2 bg-base-200 rounded-md mb-2 mt-4 ">
+        Valami
+    </div>
 </div>
