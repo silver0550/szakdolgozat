d6fc9b389adc7b88b1ed06611e4f24506290c0cc
Gacs Peter <g.silver.014@gmail.com>
2023-10-07 11:17:05 +0200

    init assignment view

--- app/Http/Livewire/Assignment.php
+++ app/Http/Livewire/Assignment.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class Assignment extends Component
 {
+//    Todo: wireable modul
     public function render()
     {
         $title = __('assignment.title');
--- resources/views/livewire/assignment.blade.php
+++ resources/views/livewire/assignment.blade.php
@@ -1,3 +1,48 @@
-<div>
-    <x-title label="{{ $title }}"/>
+<div class="h-full">
+    @isset($title)
+        <x-title label="{{ $title }}"/>
+    @endisset
+
+    <div class="flex">
+        <div class="!w-5/12 h-[500px]">
+            <Label class="label label-lg font-bold">Idő jönnek a szövegek</Label>
+            <x-table class="p-2 bg-base-200 rounded-md w-full h-full">
+                <x-slot name="head">
+                        <x-table.head class="cursor-default">{{ __('user.name')}}</x-table.head>
+                        <x-table.head class="cursor-default">{{ __('user.email') }}</x-table.head>
+                </x-slot>
+                <x-slot name='body'>
+                    @foreach(\App\Models\User::all() as $user)
+                    <tr>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                    </tr>
+                    @endforeach
+                </x-slot>
+            </x-table>
+        </div>
+        <div class="!w-1/12 h-[500px]">
+
+        </div>
+        <div class="!w-5/12 h-[500px]">
+            <Label class="label label-lg font-bold">Idő jönnek a szövegek</Label>
+            <x-table class="p-2 bg-base-200 rounded-md w-full h-full">
+                <x-slot name="head">
+                    <x-table.head class="cursor-default">{{ __('user.name')}}</x-table.head>
+                    <x-table.head class="cursor-default">{{ __('user.email') }}</x-table.head>
+                </x-slot>
+                <x-slot name='body'>
+                    @foreach(\App\Models\User::all() as $user)
+                        <tr>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                        </tr>
+                    @endforeach
+                </x-slot>
+            </x-table>
+
+        </div>
+
+
+    </div>
 </div>
